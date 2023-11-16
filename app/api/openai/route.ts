3940b7b938aca0eca7b5404c76cf52c6caa053a3
Brandon Hancock <brandon@brandonhancock.io>
2023-11-16 10:05:32 -0500

    Fix issues

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -1,19 +1,25 @@
-import OpenAI from "openai";
 import { OpenAIStream, StreamingTextResponse } from "ai";
+import { Configuration, OpenAIApi } from "openai-edge";
 
-const openai = new OpenAI({
+// Create an OpenAI API client (that's edge friendly!)
+const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+const openai = new OpenAIApi(config);
 
+// IMPORTANT! Set the runtime to edge
 export const runtime = "edge";
 
-export async function POST(req: Request) {
-  const { messages } = await req.json();
-  const response = await openai.chat.completions.create({
+export async function POST(request: Request) {
+  const { messages } = await request.json();
+
+  const response = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     stream: true,
     messages,
   });
+
   const stream = OpenAIStream(response);
+
   return new StreamingTextResponse(stream);
 }
